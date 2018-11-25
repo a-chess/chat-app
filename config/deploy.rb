1ace8b3dfc6bbba4378ea7d99a34764a6eab59c1
hyuga <hyuga3110@gmail.com>
2018-11-25 16:32:04 +0900

    linked_dirs見直し

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,8 @@ set :branch, 'master'
 set :deploy_to, '/home/hyuga/chat-app'
 set :log_level, :debug
 set :pty, true
-set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets bundle public/system public/assets}
+# set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets bundle public/system public/assets}
+set :linked_dirs, %w{log tmp/pids tmp/sockets}
 set :linked_files, %w{config/master.key}
 set :default_env, { path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH" }
 set :keep_releases, 5
