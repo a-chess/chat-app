d61140467ebc615b165ee5a8684e42c339af78ca
hyuga <hyuga3110@gmail.com>
2018-11-25 17:14:09 +0900

    test

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,7 +19,7 @@ after 'deploy:publishing' , 'deploy:restart'
 
 # deploy:assets:backup_manifestはなんかエラーになるんで実行させない。
 Rake::Task['deploy:assets:backup_manifest'].clear_actions
-# Rake::Task['deploy:assets:precompile'].clear_actions
+Rake::Task['deploy:assets:precompile'].clear_actions
 
 namespace :deploy do
   desc 'Restart application'
